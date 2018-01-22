ecc6f7a692c22e93846821ff50467e4a1358d410
Ricardo Barantini <ricardobarantini@gmail.com>
2018-01-22 14:19:33 -0200

    First commit

---
+++ client/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+
+import { Players } from './../imports/api/players';
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from './../imports/ui/AddPlayer';
+import PlayerList from './../imports/ui/PlayerList';
+
+Meteor.startup(() => {
+	Tracker.autorun(() => {
+		let players = Players.find().fetch();
+		let title = 'Score Keep';
+		let jsx = (
+		<div>
+			<TitleBar title={title} subtitle="Created by Ricardo"/>
+			<PlayerList players={players}/>
+			<AddPlayer/>		
+		</div>);
+		ReactDOM.render(jsx, document.getElementById('app'));
+	});
+});
---
+++ imports/ui/PlayerList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Player } from './Player';
+
+export default class PlayerList extends React.Component {
+	renderPlayers() {
+		// return this.props.players.map((player) => <Player key={player._id} player={player}/>);
+		return this.props.players.map((player) => {
+			return <Player key={player._id} player={player}/>;
+		});
+	}
+
+	render() {
+		// return (<div>{this.renderPlayers()}</div>);
+		return (
+			<div>
+				{this.renderPlayers()}
+			</div>
+		);
+	}
+};
+
+PlayerList.propTypes = {
+	players: PropTypes.array.isRequired
+};
